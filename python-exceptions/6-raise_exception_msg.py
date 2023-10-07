1147d62ecfaaf5de80f43345f83413bcdad645c2
Holbiwan <6188@holbertonstudents.com>
2023-10-07 11:33:48 +0200

    Fix code 6-raise_exception_msg.py

--- python-exceptions/6-raise_exception_msg.py
+++ python-exceptions/6-raise_exception_msg.py
@@ -1,4 +1,5 @@
- #!/usr/bin/python3
-
-def raise_exception_msg(message=""):
-    raise NameError(message)
+#!/usr/bin/python3
+str1 = "Holberton"
+str2 = "School"
+str1 += " " + str2
+print(f"Welcome to {str1}!")
