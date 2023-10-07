2825959a7dfbb53965b6da1fd6d2761dc4859110
Holbiwan <6188@holbertonstudents.com>
2023-10-07 11:06:38 +0200

    2-safe_print_list_integers.py

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
-print("\"Programming is like building a multilingual puzzle")
+def safe_print_list_integers(my_list=[], x=0):
+    counter = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            counter += 1
+        except (TypeError, ValueError):
+            pass
+    print()
+    return counter
