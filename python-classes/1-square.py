9542ff56c97e7907c0ed0893456ddb2d8e8e6fe8
Holbiwan <6188@holbertonstudents.com>
2023-10-10 10:40:33 +0200

    Create 1-square.py file

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""A class called square is initialized"""
+
+
+class Square:
+    """Represents a square"""
+
+    def __init__(self, size):
+        """size: length of side of the square"""
+
+        self.__size = size
+    pass
