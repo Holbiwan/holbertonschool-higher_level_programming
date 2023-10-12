7ce9145cebe2e2f19b9527dae3906b6b20041732
Holbiwan <6188@holbertonstudents.com>
2023-10-12 06:22:50 +0200

    Fix code 4-square.py file

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,15 +1,26 @@
 #!/usr/bin/python3
-'''function print #'''
+'''a class called square is initialized'''
 
 
-def print_square(size):
-    '''validations'''
-    if not isinstance(size, int):
-        raise TypeError("size must be an integer")
-    elif size < 0:
-        raise ValueError("size must be >= 0")
-    elif isinstance(size, float) and size < 0:
-        raise TypeError("size must be an integer")
-    '''print #'''
-    for i in range(size):
-        print("#" * size)
+class Square:
+    '''a private instance is made to an attribute with the name size'''
+    def __init__(self, size=0):
+        self.__size = size
+    '''private instance of the attribute is returned'''
+    @property
+    def get_size(self):
+        return self.__size
+
+    @get_size.setter
+    def size(self, value):
+        '''raise an error if the data type is incorrect'''
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        '''raise an error if the data is less that 0'''
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        '''is instantiated with the value'''
+        self.__size = value
+    '''returns the current square area'''
+    def area(self):
+        return self.__size * self.__size
