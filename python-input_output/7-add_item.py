64f87bf096e9d42d8ad98873c8bec1f6b17c1ab5
Holbiwan <6188@holbertonstudents.com>
2023-10-20 17:14:57 +0200

    Create 7-add_item.py file

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+'''import'''
+import json
+'''creates an object from a json'''
+
+
+def load_from_json_file(filename):
+    '''open json'''
+    with open(filename, "r") as file:
+        '''load json'''
+        data = json.loads(file.read())
+    '''return'''
+    return data#!/usr/bin/python3
+'''import'''
+import sys
+import os.path
+
+'''import json'''
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+'''variable'''
+args = sys.argv
+filename = 'add_item.json'
+'''function'''
+
+
+def add_list():
+    '''variable'''
+    obj_list = []
+    '''validation'''
+    with open(filename, "a") as file:
+        pass
+    if os.stat(filename).st_size == 0:
+        obj_list
+    elif os.path.exists(filename):
+        obj_list = load_from_json_file(filename)
+    '''add args'''
+    obj_list.extend(args[1:])
+    '''save list'''
+    save_to_json_file(obj_list, filename)
+'''function'''
+if __name__ == "__main__":
+    add_list()
