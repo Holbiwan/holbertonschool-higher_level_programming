44959953df3b5658b97df34149b4fb5c1bc556da
Holbiwan <6188@holbertonstudents.com>
2023-11-22 18:55:18 +0100

    Modify ORDER BY line

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -2,6 +2,7 @@
 -- Each record display: tv_shows.title - tv_show_genres_id
 -- Results are sorted in ascending order by tv_shows.title and tv_show doesn't have a genre, it displays NULL
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_show_genres
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_genres.show_id
-ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
+FROM tv_shows
+LEFT JOIN tv_show_genres 
+ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
