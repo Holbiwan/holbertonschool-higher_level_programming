48a11521ea32e7364b09c4591684f74ce52cce19
Holbiwan <6188@holbertonstudents.com>
2023-11-26 10:52:08 +0100

    Modif following check failed

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-The script lists all states from the
+This script lists all states from the
 database `hbtn_0e_0_usa`.
 """
 
@@ -8,10 +8,11 @@ import MySQLdb
 from sys import argv
 
 if __name__ == '__main__':
-    
-    """Access to the database and get the states"""
-   
-       db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+    """
+    Access to the database and get the states
+    from the database.
+    """
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
                          passwd=argv[2], db=argv[3])
 
     cur = db.cursor()
