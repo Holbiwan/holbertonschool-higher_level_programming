cb4599bd70365808e94052da6ed496448ef710f9
Holbiwan <6188@holbertonstudents.com>
2023-11-26 19:41:40 +0100

    Review code

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,36 +1,28 @@
 #!/usr/bin/python3
-""" Select states with names matching arguments """
+"""
+    Script that lists all cities from the database hbtn_0e_4_usa
+"""
+import MySQLdb
+import sys
+
 
 if __name__ == '__main__':
-    """Check if the script is being run directly"""
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3606,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
 
-    """Import necessary modules"""
-    from sys import argv
-    import MySQLdb
+    cursor = db.cursor()
+    cursor.execute("SELECT cities.id, cities.name, states.name "
+                "FROM cities, states WHERE cities.state_id = states.id "
+                "ORDER BY cities.id ASC")
 
-    """Extract MySQL username, password, and database name from command line arguments"""
-    db_user = argv[1]
-    db_passwd = argv[2]
-    db_name = argv[3]
-
-    """Connect to the MySQL server running on localhost at port 3306"""
-    database = MySQLdb.connect(host='localhost',
-                               port=3306,
-                               user=db_user,
-                               passwd=db_passwd,
-                               db=db_name)
-
-    """Create a cursor object to interact with the database"""
-    cursor = database.cursor()
-
-    """Execute an SQL query to select cities.id, cities.name, and states.name from the 'cities' table
-    joined with the 'states' table on cities.state_id = states.id, ordered by cities.id in ascending order"""
-    cursor.execute('SELECT cities.id, cities.name, states.name FROM cities\
-                   JOIN states\
-                   ON cities.state_id = states.id\
-                   ORDER BY cities.id ASC')
-
-    """Iterate through the result set"""
-    for row in cursor.fetchall():
-        """Print each row"""
+    cities = cursor.fetchall()
+    for row in cities:
         print(row)
+
+    cursor.close()
+    db.close()
