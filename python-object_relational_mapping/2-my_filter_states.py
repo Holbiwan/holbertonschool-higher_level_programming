ba3a1d034d6ade9a3a689498cedcd0b655b5d392
Holbiwan <6188@holbertonstudents.com>
2023-11-26 17:59:30 +0100

    Fix database name

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,28 +1,38 @@
 #!/usr/bin/python3
-"""
-This script takes in an argument and
-displays all values in the states
-where name matches the argument
-from the database hbtn_0e_0_usa
-"""
-
-import MySQLdb
-from sys import argv
+""" Select states with names matching arguments """
 
 if __name__ == '__main__':
-    """
-    Access to the database and get the states
-    from the database.
-    """
+    """Check if the script is being run directly"""
 
-    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
-                         passwd=argv[2], db=argv[3])
+    """Import necessary modules"""
+    from sys import argv
+    import MySQLdb
 
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states \
-                 WHERE name LIKE BINARY '{}' \
-                 ORDER BY states.id ASC".format(argv[4]))
-    rows = cursor.fetchall()
+    """Extract MySQL username, password, database name, and search term from command line arguments"""
+    db_user = argv[1]
+    db_passwd = argv[2]
+    db_name = argv[3]
+    search = argv[4]
 
-    for row in rows:
-        print(row)
+    """Connect to the MySQL server running on localhost at port 3306"""
+    database = MySQLdb.connect(host='localhost',
+                               port=3306,
+                               user=db_user,
+                               passwd=db_passwd,
+                               db=db_name)
+
+    """Create a cursor object to interact with the database"""
+    cursor = database.cursor()
+
+    """Execute an SQL query to select id and name from the 'states' table
+    where the name matches the specified search term, ordered by id in ascending order"""
+    cursor.execute('SELECT id, name FROM states\
+                   WHERE states.name = \'{}\'\
+                   ORDER BY states.id ASC'.format(search))
+
+    """Iterate through the result set"""
+    for row in cursor.fetchall():
+        """Check if the name of the state matches the search term"""
+        if row[1] == search:
+            """Print the row if the condition is met"""
+            print(row)
