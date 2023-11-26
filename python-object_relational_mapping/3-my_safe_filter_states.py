e46e0f6c93e301f593f43bbfbd6a69348468aa05
Holbiwan <6188@holbertonstudents.com>
2023-11-26 18:02:24 +0100

    Review code

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,42 +1,38 @@
 #!/usr/bin/python3
-"""
-This script takes in an argument and
-displays all values in the states
-where name matches the argument
-from the database hbtn_0e_0_usa
-
-The script is safe from
-MySQL injections!
-"""
-
-import MySQLdb
-from sys import argv
+""" Select states with names matching arguments """
 
 if __name__ == '__main__':
-    """
-    Access to the database and get the states
-    from the database.
-    """
+    """Check if the script is being run directly"""
 
-    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
-                         passwd=argv[2], db=argv[3])
+    """Import necessary modules"""
+    from sys import argv
+    import MySQLdb
 
-    with db.cursor() as cursor:
-        cursor.execute("""
-            SELECT
-                *
-            FROM
-                states
-            WHERE
-                name LIKE BINARY %(name)s
-            ORDER BY
-                states.id ASC
-        """, {
-            'name': argv[4]
-        })
+    """Extract MySQL username, password, database name, and search term from command line arguments"""
+    db_user = argv[1]
+    db_passwd = argv[2]
+    db_name = argv[3]
+    
+    # Format the search term from command line argument
+    search = '{}'.format(argv[4])
 
-        rows = cursor.fetchall()
+    """Connect to the MySQL server running on localhost at port 3306"""
+    database = MySQLdb.connect(host='localhost',
+                               port=3306,
+                               user=db_user,
+                               passwd=db_passwd,
+                               db=db_name)
 
-    if rows is not None:
-        for row in rows:
-            print(row)
+    """Create a cursor object to interact with the database"""
+    cursor = database.cursor()
+
+    """Execute an SQL query to select id and name from the 'states' table
+    where the name matches the specified search term, ordered by id in ascending order"""
+    cursor.execute('SELECT id, name FROM states\
+                   WHERE name = %s\
+                   ORDER BY states.id ASC;', (search,))
+
+    """Iterate through the result set"""
+    for row in cursor.fetchall():
+        """Print each row"""
+        print(row)
