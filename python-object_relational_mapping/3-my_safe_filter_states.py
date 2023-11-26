34ff329ff5a897301a04ea7ee6ebd621dc5a5097
Holbiwan <6188@holbertonstudents.com>
2023-11-26 18:51:24 +0100

    Update state_name_searched

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,38 +1,29 @@
 #!/usr/bin/python3
-""" Select states with names matching arguments """
+"""
+    Script that takes in arguments and displays all values in the states
+    table of hbtn_0e_0_usa where name matches the argument,
+    this time, write one that is safe from MySQL injections
+"""
+import MySQLdb
+import sys
+
 
 if __name__ == '__main__':
-    """Check if the script is being run directly"""
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+    state_name_searched = sys.argv[4]
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name = %s "
+                "ORDER BY states.id ASC", (state_name_searched, ))
 
-    """Import necessary modules"""
-    from sys import argv
-    import MySQLdb
-
-    """Extract MySQL username, password, database name, and search term from command line arguments"""
-    db_user = argv[1]
-    db_passwd = argv[2]
-    db_name = argv[3]
-    
-    # Format the search term from command line argument
-    search = '{}'.format(argv[4])
-
-    """Connect to the MySQL server running on localhost at port 3306"""
-    database = MySQLdb.connect(host='localhost',
-                               port=3306,
-                               user=db_user,
-                               passwd=db_passwd,
-                               db=db_name)
-
-    """Create a cursor object to interact with the database"""
-    cursor = database.cursor()
-
-    """Execute an SQL query to select id and name from the 'states' table
-    where the name matches the specified search term, ordered by id in ascending order"""
-    cursor.execute('SELECT id, name FROM states\
-                   WHERE name = %s\
-                   ORDER BY states.id ASC;', (search,))
-
-    """Iterate through the result set"""
-    for row in cursor.fetchall():
-        """Print each row"""
+    query_rows = cur.fetchall()
+    for row in query_rows:
         print(row)
+
+    cur.close()
+    db.close()
