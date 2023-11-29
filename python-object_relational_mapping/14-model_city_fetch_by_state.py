ee2f4599cf02e8608f00c2c457092a8d5fbb58b2
Holbiwan <6188@holbertonstudents.com>
2023-11-29 13:39:16 +0100

    Re-push after fix city.py file

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,35 +1,33 @@
 #!/usr/bin/python3
 """Prints all City objects from the database hbtn_0e_14_usa"""
- 
+
 import sys
-from model_state import Base, State
-from model_city import Base, City
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from model_city import City
 
 if __name__ == '__main__':
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
 
+    username, password, database = sys.argv[1:4]
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-                           format(argv[1], argv[2], argv[3],
-                                  pool_pre_ping=True))
-    """Etablishing a connection to the MySQL server"""
+                           format(username, password, database),
+                           pool_pre_ping=True)
+    """Establishing a connection to the MySQL server"""
 
     Session = sessionmaker(bind=engine)
     session = Session()
     """Creating a session for database interaction"""
-    
 
-    query_lines = session.query(City, State).\
-        filter(City.state_id == State.id).all()
-    """Retrieving City-State pairs objects with matching stade_id"""
-      
+    query_result = session.query(City, State).filter(City.state_id == State.id).all()
+    """Retrieving City-State pairs with matching state_id"""
 
-    for city, state in query_lines:
-        """Show the results"""
+    for city, state in query_result:
+        """Displaying the results"""
         print("{}: ({}) {}".format(state.name, city.id, city.name))
 
     session.close()
