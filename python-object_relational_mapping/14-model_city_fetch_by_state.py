6c37a3e24cfa7b96dd746d4c6ee9c5b0dea6961e
Holbiwan <6188@holbertonstudents.com>
2023-11-29 13:21:05 +0100

    Create 14-model_city_fetch_by_state.py file

---
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Prints all City objects from the database hbtn_0e_14_usa"""
+ 
+import sys
+from model_state import Base, State
+from model_city import Base, City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(argv[1], argv[2], argv[3],
+                                  pool_pre_ping=True))
+    """Etablishing a connection to the MySQL server"""
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    """Creating a session for database interaction"""
+    
+
+    query_lines = session.query(City, State).\
+        filter(City.state_id == State.id).all()
+    """Retrieving City-State pairs objects with matching stade_id"""
+      
+
+    for city, state in query_lines:
+        """Show the results"""
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
+
+    session.close()
+    """Close the session"""
