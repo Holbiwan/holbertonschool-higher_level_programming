1a382cc3c1d609c16e477a9b0ccbd2054356266c
Holbiwan <sabrinapapeau@gmail.com>
2024-02-02 15:37:54 +0100

    Create 6-script.js file

---
+++ javascript-dom_manipulation/6-script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const characterElement = document.getElementById('character');
+    const apiURL = 'https://swapi-api.hbtn.io/api/people/5/?format=json';
+
+    fetch(apiURL)
+        .then(response => response.json())
+        .then(data => characterElement.textContent = data.name)
+        .catch(error => console.error('Error obtaining data:', error));
+
+});
