8a9f35bd3a3465e0345de38709840814928bc090
Holbiwan <sabrinapapeau@gmail.com>
2024-02-02 15:33:13 +0100

    Update 5-script.js file

---
+++ javascript-dom_manipulation/5-script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const updateHeaderElement = document.getElementById("update_header");
+    const headerElement = document.querySelector("header");
+
+    updateHeaderElement.addEventListener("click", () => headerElement.textContent = "New Header!!!");
+});
