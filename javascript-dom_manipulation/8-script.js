bc2fa2a26670f6f79323ab2e030489d405da602f
Holbiwan <sabrinapapeau@gmail.com>
2024-02-02 16:46:10 +0100

    Replace var by const following incorrect output

--- javascript-dom_manipulation/8-script.js
+++ javascript-dom_manipulation/8-script.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var helloElement = document.getElementById("hello");
-    var apiUrl = "https://hellosalut.stefanbohacek.dev/?lang=fr";
+    const helloElement = document.getElementById("hello");
+    const apiUrl = "https://hellosalut.stefanbohacek.dev/?lang=fr";
 
     fetch(apiUrl)
-      .then(response => response.text())
+      .then(response => response.json())  // Change this line to parse the response as JSON
       .then(data => {
-        helloElement.textContent = data;
+        helloElement.innerText = data.hello;  // Access the 'hello' property in the JSON data
       })
       .catch(error => {
         console.error("Error obtaining translation:", error);
