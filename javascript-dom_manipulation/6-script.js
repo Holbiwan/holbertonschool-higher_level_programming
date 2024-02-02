a30e9119383e23df5368341424b12537776af3bd
Holbiwan <sabrinapapeau@gmail.com>
2024-02-02 18:26:09 +0100

    Fix semistandard syntax

--- javascript-dom_manipulation/6-script.js
+++ javascript-dom_manipulation/6-script.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const characterElement = document.getElementById('character');
-    const apiURL = 'https://swapi-api.hbtn.io/api/people/5/?format=json';
+  const characterElement = document.getElementById('character');
+  const apiURL = 'https://swapi-api.hbtn.io/api/people/5/?format=json';
 
-    fetch(apiURL)
-        .then(response => response.json())
-        .then(data => {
-            characterElement.textContent = data.name;
-        })
-        .catch(error => console.error('Error obtaining data:', error));
+  fetch(apiURL)
+    .then(response => response.json())
+    .then(data => {
+      characterElement.textContent = data.name;
+    })
+    .catch(error => console.error('Error obtaining data:', error));
 });
