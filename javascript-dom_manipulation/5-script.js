ccb5a2c8bb5c08a84f476314d75a3a8743dd9d74
Holbiwan <sabrinapapeau@gmail.com>
2024-02-02 18:29:05 +0100

    Fix semistanrd syntax

--- javascript-dom_manipulation/5-script.js
+++ javascript-dom_manipulation/5-script.js
@@ -1,6 +1,6 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const updateHeaderElement = document.getElementById("update_header");
-    const headerElement = document.querySelector("header");
+document.addEventListener('DOMContentLoaded', () => {
+  const updateHeaderElement = document.getElementById('update_header');
+  const headerElement = document.querySelector('header');
 
-    updateHeaderElement.addEventListener("click", () => headerElement.textContent = "New Header!!!");
+  updateHeaderElement.addEventListener('click', () => headerElement.textContent = 'New Header!!!');
 });
